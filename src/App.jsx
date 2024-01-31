d9227e86e6f727d2c501ea2285d4e5b743b9e421
haker <whitehaker010@gmail.com>
2024-01-31 13:28:08 +0500

    main

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from '@material-tailwind/react'
+
+function App() {
+
+  return (
+    <>
+      <Button className=''> Hello Worlds</Button>
+    </>
+  )
+}
+
+export default App
