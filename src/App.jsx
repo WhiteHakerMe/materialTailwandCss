50d39255fc076fcbcb14550ae6704549d71938cc
haker <140898334+WhiteHakerMe@users.noreply.github.com>
2024-02-05 15:06:58 +0500

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+import { createBrowserRouter } from 'react-router-dom'
 import './App.css'
 import { Button } from '@material-tailwind/react'
+import Home from './pages/home'
+import Products from './pages/products'
+import Info from './pages/info'
 
 function App() {
-
+  const routers = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />
+    },
+    {
+      path: "/products",
+      element: <Products />
+    },
+    {
+      path: "/info",
+      element: <Info />
+    }
+  ])
   return (
     <>
       <Button className=' bg-black text-white'> Hello Worlds</Button>
